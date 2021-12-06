8fdae2f6b94533113c6c0964df2b47f859c9f32b
Pritesh Tupe <priteshtupe@gmail.com>
2021-12-06 21:08:02 +0530

    Add tabs to open with firefox

--- autostart/.config/autostart/open-default-windows.sh
+++ autostart/.config/autostart/open-default-windows.sh
@@ -1,13 +1,5 @@
 #!/usr/bin/env bash
 
-# kitty --start-as=maximized --name=bpytop bpytop & disown
-# sleep 1
-# xdotool set_desktop_for_window $(xdotool search "bpytop") 8 > /dev/null
-
-# kitty --start-as=maximized --name=mocp mocp & disown
-# sleep 1
-# xdotool set_desktop_for_window $(xdotool search "mocp") 7 > /dev/null
-
 kitty --start-as=maximized --name=wiki nvim -S ~/MEGASync/wiki.vim & disown
 sleep 1
 xdotool set_desktop_for_window $(xdotool search "wiki") 7 > /dev/null
@@ -16,4 +8,4 @@ kitty --start-as=maximized --name=agenda nvim -S ~/MEGASync/agenda.vim & disown
 sleep 1
 xdotool set_desktop_for_window $(xdotool search "agenda") 6 > /dev/null
 
-firefox > /dev/null 2>&1 & disown
+firefox "https://mail.google.com/mail/u/0/#inbox" "https://calendar.google.com/calendar/u/0/r" "https://tasksboard.com/app" > /dev/null 2>&1 & disown
