5d80bbf45146c5261401aa5b4f44c5b8d5a58ada
Pritesh Tupe <priteshtupe@gmail.com>
2021-12-26 18:43:15 +0530

    Add script to update learning repos

---
+++ scripts/.local/bin/update-all-learn-repos.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# This script pulls latest commits for all the repos
+# inside ~/Project/learn directory
+
+pushd /home/pritesh/Projects/learn > /dev/null
+
+# Note: the '\' in '\ls' is to use original ls command
+# since I have it aliased to exa.
+for dir in $(\ls -d */); do
+  pushd $dir > /dev/null
+  echo "Updating $dir..."
+  git pull > /dev/null
+  popd > /dev/null
+done
+echo "All Done!"
+popd > /dev/null
