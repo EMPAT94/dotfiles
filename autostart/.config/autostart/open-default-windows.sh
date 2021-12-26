337bc8f6e8426cc7441e0e162ad77d929beeb304
Pritesh Tupe <priteshtupe@gmail.com>
2021-12-26 21:16:14 +0530

    Change startup apps

--- autostart/.config/autostart/open-default-windows.sh
+++ autostart/.config/autostart/open-default-windows.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/env bash
-
-kitty --start-as=maximized --name=wiki nvim -S ~/MEGASync/wiki.vim & disown
+kitty & disown
 sleep 1
-xdotool set_desktop_for_window $(xdotool search "wiki") 7 > /dev/null
-
-kitty --start-as=maximized --name=agenda nvim -S ~/MEGASync/agenda.vim & disown
-sleep 1
-xdotool set_desktop_for_window $(xdotool search "agenda") 6 > /dev/null
-
-firefox "https://mail.google.com/mail/u/0/#inbox" "https://calendar.google.com/calendar/u/0/r" "https://tasksboard.com/app" > /dev/null 2>&1 & disown
+xdotool set_desktop 6
+brave "https://mail.google.com/mail/u/0/#inbox" "https://calendar.google.com/calendar/u/0/r" "https://tasksboard.com/app" > /dev/null 2>&1 & disown
